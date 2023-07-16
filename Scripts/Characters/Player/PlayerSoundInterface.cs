9df7765452190761318124e523bc18b6d964bc9c
Colin2222 <crlew@outlook.com>
2023-07-16 19:45:54 -0400

    variable footstep volume, running jump sound

--- Scripts/Characters/Player/PlayerSoundInterface.cs
+++ Scripts/Characters/Player/PlayerSoundInterface.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerSoundInterface : MonoBehaviour
 {
+	public Rigidbody2D rb;
+	public float footstepVolumeTopSpeed;
+	
     public AudioSource step1;
     public AudioSource step2;
+	public AudioSource runningJump;
+	public AudioSource stillJump;
+	public AudioSource stillJumpLand;
+	public AudioSource runningJumpLand;
+	public AudioSource cornerGrab;
+	public AudioSource cornerClimb;
+	public AudioSource wallImpact;
+	public AudioSource wallJump;
 	
 	public void PrintEvent(){
 		Debug.Log("STEP");
 	}
 	
 	public void PlayStep1(){
+		step1.volume = Mathf.Abs(rb.velocity.x / footstepVolumeTopSpeed);
 		step1.Play();
 	}
 	
 	public void PlayStep2(){
+		step2.volume = Mathf.Abs(rb.velocity.x / footstepVolumeTopSpeed);
 		step2.Play();
+	}
+	
+	public void PlayRunningJump(){
+		runningJump.Play();
 	}
 }
