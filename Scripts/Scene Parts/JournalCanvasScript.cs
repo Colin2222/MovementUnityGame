0ecc56d521e62edb8a73053a693ccf4444608496
Colin2222 <crlew@outlook.com>
2023-12-31 02:09:34 -0600

    initial displaying of page contents

--- Scripts/Scene Parts/JournalCanvasScript.cs
+++ Scripts/Scene Parts/JournalCanvasScript.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JournalCanvasScript : MonoBehaviour
 {
 	public GameObject journalObject;
+	public Image writingImage; 
+	
+	public void SetPageImages(Sprite writingSprite){
+		writingImage.sprite = writingSprite;
+	}
 }
--- Scripts/Scene Parts/JournalManager.cs
+++ Scripts/Scene Parts/JournalManager.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Newtonsoft.Json;
 
 public class JournalManager : MonoBehaviour
 {
 	public JournalCanvasScript journalUI;
-	int currentPage;
+	int currentPage = 0;
+	JournalEntrySet es;
+	AsyncOperationHandle<Sprite> writingSpriteOp;
+	Sprite writingSprite;
 	
 	[System.NonSerialized]
 	public bool active = false;
+	
+	TextAsset currentDataTxt;
 	
     // Start is called before the first frame update
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
 		
-		// load in persistent journal data
+		// load in json of journal data
+		var operation = Addressables.LoadAssetAsync<TextAsset>("Assets/Data/journal_data.json");
+		currentDataTxt = operation.WaitForCompletion();
+		
+		// parse json into cutscene object
+		es = JsonConvert.DeserializeObject<JournalEntrySet>(currentDataTxt.text);
+		
+		// done parsing json, release asset out of memory
+		Addressables.Release(operation);
     }
 
     // Update is called once per frame
@@ -26,9 +42,16 @@ public class JournalManager : MonoBehaviour
 	
 	public void Activate(){
 		journalUI.journalObject.SetActive(true);
+		writingSpriteOp = Addressables.LoadAssetAsync<Sprite>("Assets/Data/JournalPages/" + es.entries[0].writingImageLocation + ".png");
+		writingSprite = writingSpriteOp.WaitForCompletion();
+		
+		// set sprite
+		journalUI.SetPageImages(writingSprite);
 	}
 	
 	public void Deactivate(){
+		Addressables.Release(writingSpriteOp);
+		
 		journalUI.journalObject.SetActive(false);
 	}
 	
@@ -38,9 +61,13 @@ public class JournalManager : MonoBehaviour
 }
 
 public class JournalEntry{
-	int number;
-	string textImageLocation;
-	string drawingImageLocation;
-	bool found;
-	bool completed;
+	public int number;
+	public string writingImageLocation;
+	public string drawingImageLocation;
+	public bool found;
+	public bool completed;
+}
+
+public class JournalEntrySet{
+	public JournalEntry[] entries;
 }
