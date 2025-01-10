a0bc743a8a24127d57cabec6a5418fecfc3935d3
Colin2222 <crlew@outlook.com>
2025-01-09 23:52:56 -0500

    basecamp river + sounds

---
+++ Scripts/Scene Parts/Sounds/SoundRepeater.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRepeater : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float repeatTime;
+    public float startTime;
+
+    public bool loopingOnStart;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!loopingOnStart){
+            timer += Time.deltaTime;
+            if(timer >= repeatTime){
+                audioSource.Play();
+                timer = 0;
+            }
+        }
+    }
+}
