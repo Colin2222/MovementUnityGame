93fad37752c7a72c0f8fc92147e9a40c2a1b8f48
Colin2222 <crlew@outlook.com>
2025-09-12 23:43:03 -0400

    pedestal site

---
+++ Scripts/Scene Parts/Interactables/Sites/SitePedestal.cs
@@ -0,0 +1,238 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SitePedestal : Site
+{
+    InventoryCanvasScript canvas;
+    (int x, int y) currentSlotPos;
+    (int x, int y) selectionPos;
+    bool inSelection = false;
+    Inventory playerInventory;
+    public SitePedestalPanel sitePanel;
+    public Inventory siteInventory;
+
+    // can be player or site
+    string currentInventory = "player";
+    string currentSelection = "none";
+
+    // sprite to display held item
+    public SpriteRenderer displaySprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sitePanel.SyncInventory(siteInventory);
+        UpdateIcons();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void EnterRange()
+    {
+
+    }
+
+    protected override void ExitRange()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        GameObject canvasObj = GameObject.FindWithTag("InventoryUI");
+        if (canvasObj != null)
+        {
+            canvas = canvasObj.GetComponent<InventoryCanvasScript>();
+            canvas.openBagObject.SetActive(true);
+            canvas.itemsObject.SetActive(true);
+            currentSelection = "player";
+            currentInventory = "player";
+            currentSlotPos = (0, 0);
+            canvas.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            PlayerHub.Instance.inventoryHandler.UpdateIcons();
+            UpdateIcons();
+            playerInventory = PlayerHub.Instance.inventoryHandler.inventory;
+
+            // activate site panel
+            sitePanel.gameObject.SetActive(true);
+
+            // activate camera
+            ActivateCamera();
+        }
+    }
+
+    public override void LeaveInteraction()
+    {
+        currentSlotPos = (0, 0);
+        canvas.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+        sitePanel.ChangeSelection(-1, -1);
+        currentSelection = "player";
+        inSelection = false;
+        sitePanel.EndSelection();
+        canvas.EndSelection();
+        canvas.itemsObject.SetActive(false);
+        canvas.openBagObject.SetActive(false);
+        sitePanel.gameObject.SetActive(false);
+
+        // deactivate camera
+        DeactivateCamera();
+    }
+    
+    public override void MenuUp(){
+        if(currentInventory == "player"){
+            if(currentSlotPos.y > 0){
+                (currentSlotPos.y)--;
+                canvas.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+        } else if(currentInventory == "site"){
+            if(currentSlotPos.y > 0){
+                (currentSlotPos.y)--;
+                sitePanel.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+        }
+    }
+
+    public override void MenuDown(){
+        if(currentInventory == "player"){
+            if(currentSlotPos.y < playerInventory.height - 1){
+                (currentSlotPos.y)++;
+                canvas.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+        } else if(currentInventory == "site"){
+            // LEAVE AN EXTRA SLOT FOR THE BUTTON AT THE BOTTOM
+            if(currentSlotPos.y < siteInventory.height - 1){
+                (currentSlotPos.y)++;
+                sitePanel.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+        }
+    }
+
+    public override void MenuLeft(){
+        if(currentInventory == "player"){
+            if(currentSlotPos.x > 0){
+                (currentSlotPos.x)--;
+                canvas.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+        } else if(currentInventory == "site"){
+            if(currentSlotPos.x > 0){
+                (currentSlotPos.x)--;
+                sitePanel.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            } else{
+                currentInventory = "player";
+                sitePanel.ChangeSelection(-1, -1);
+                if(currentSlotPos.y >= playerInventory.height){
+                    currentSlotPos.y = playerInventory.height - 1;
+                }
+                canvas.ChangeSelection(currentSlotPos.y, playerInventory.width - 1);
+                currentSlotPos.x = playerInventory.width - 1;
+            }
+        }
+    }
+
+    public override void MenuRight(){
+        if(currentInventory == "player"){
+            if(currentSlotPos.x < playerInventory.width - 1){
+                (currentSlotPos.x)++;
+                canvas.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            } else{
+                currentInventory = "site";
+                canvas.ChangeSelection(-1, -1);
+                if(currentSlotPos.y >= siteInventory.height){
+                    currentSlotPos.y = siteInventory.height - 1;
+                }
+                sitePanel.ChangeSelection(currentSlotPos.y, 0);
+                currentSlotPos.x = 0;
+            }
+        }  else if(currentInventory == "site"){
+            if(currentSlotPos.x < siteInventory.width - 1){
+                (currentSlotPos.x)++;
+                sitePanel.ChangeSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+        }
+    }
+    
+    public override void MenuSelect(){
+        Inventory selectedInv = currentSelection == "player" ? playerInventory : siteInventory;
+        Inventory currentInv = currentInventory == "player" ? playerInventory : siteInventory;
+
+        if(inSelection){
+            if(selectedInv != currentInv || selectionPos != currentSlotPos){
+                currentInv.InsertItem(selectedInv, selectionPos.x, selectionPos.y, currentSlotPos.x, currentSlotPos.y);
+            }
+            EndSelection();
+		} else if (currentInv.contents[currentSlotPos.y, currentSlotPos.x] != null){
+            currentSelection = currentInventory;
+            selectionPos = currentSlotPos;
+            inSelection = true;
+            if(currentInventory == "site"){
+                sitePanel.StartSelection(currentSlotPos.y, currentSlotPos.x);
+            } else if(currentInventory == "player"){
+                canvas.StartSelection(currentSlotPos.y, currentSlotPos.x);
+            }
+		}
+    }
+
+    public override void MenuInteract()
+    {
+
+    }
+    
+    void EndSelection(){
+        UpdateIcons();
+        PlayerHub.Instance.inventoryHandler.UpdateIcons();
+        sitePanel.EndSelection();
+        canvas.EndSelection();
+        inSelection = false;
+    }
+
+    public void UpdateIcons()
+    {
+        for (int i = 0; i < siteInventory.height; i++)
+        {
+            for (int j = 0; j < siteInventory.width; j++)
+            {
+                if (siteInventory.contents[i, j] != null)
+                {
+                    sitePanel.SetIcon(i, j, siteInventory.contents[i, j].item.icon, siteInventory.contents[i, j].quantity);
+                }
+                else
+                {
+                    sitePanel.SetIcon(i, j, null, 0);
+                }
+            }
+        }
+
+        // set the display sprite
+        if(siteInventory.contents[0, 0] != null){
+            displaySprite.sprite = siteInventory.contents[0, 0].item.icon;
+        } else{
+            displaySprite.sprite = null;
+        }
+    }
+
+    public override void LoadSite(SavedSite savedSite)
+    {
+        // populate inventory
+        SavedInventory inv = savedSite.inventories[0];
+        SaveDataHelperMethods.LoadInventory(siteInventory, inv);
+        siteInventory.maxQuantities = new int[siteInventory.height, siteInventory.width];
+        siteInventory.maxQuantities[0, 0] = 1;
+    }
+
+    public override SavedSite SaveSite(){
+        SavedSite savedSite = new SavedSite();
+        savedSite.name = "pedestal";
+        savedSite.additional_data = new Dictionary<string, string>();
+        savedSite.inventories = new List<SavedInventory>();
+        savedSite.inventories.Add(siteInventory.SaveInventory());
+        return savedSite;
+    }
+
+    public override void ConstructSite(){
+        siteInventory.ResetInventory(siteInventory.width, siteInventory.height);
+    }
+}
---
+++ Scripts/Scene Parts/Interactables/Sites/SitePedestalPanel.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SitePedestalPanel : MonoBehaviour
+{
+    public Transform items;
+    public GameObject itemsObject;
+
+    GameObject[,] itemSlots;
+    GameObject buttonObject;
+
+    public float slotSize;
+    public GameObject slotPrefab;
+    public Color defaultSlotColor;
+    public Color currentSlotColor;
+
+    (int y, int x) selection;
+    GameObject selectionSlot;
+    public Color selectionSlotColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeSelection(int y, int x){
+        itemSlots[selection.y, selection.x].GetComponent<SpriteRenderer>().color = defaultSlotColor;
+		if(y >= 0 && x >= 0){
+			itemSlots[y, x].GetComponent<SpriteRenderer>().color = currentSlotColor;
+			selection = (y, x);
+		}
+	}
+
+	public void StartSelection(int y, int x){
+		selectionSlot.transform.localPosition = new Vector3(x * slotSize, -y * slotSize, 0);
+		selectionSlot.SetActive(true);
+	}
+
+	public void EndSelection(){
+		selectionSlot.SetActive(false);
+	}
+
+    public void SetIcon(int y, int x, Sprite icon, int quantity){
+		itemSlots[y, x].transform.GetChild(0).gameObject.SetActive(icon != null);
+		itemSlots[y, x].transform.GetChild(1).gameObject.SetActive(icon != null);
+		itemSlots[y, x].transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text = icon == null ? "" : quantity.ToString();
+		itemSlots[y, x].transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = icon;
+	}
+
+    public void SyncInventory(Inventory inventory)
+    {
+        itemSlots = new GameObject[inventory.height, inventory.width];
+        for (int i = 0; i < inventory.height; i++)
+        {
+            for (int j = 0; j < inventory.width; j++)
+            {
+                itemSlots[i, j] = Instantiate(slotPrefab, items, false);
+                itemSlots[i, j].GetComponent<Transform>().localScale = new Vector3(slotSize, slotSize, 1);
+                itemSlots[i, j].GetComponent<SpriteRenderer>().color = defaultSlotColor;
+                itemSlots[i, j].transform.localPosition = new Vector3(j * slotSize, -i * slotSize, 0);
+                itemSlots[i, j].name = "Slot[" + i + "," + j + "]";
+            }
+        }
+
+		// initiate selection slot
+		selectionSlot = Instantiate(slotPrefab, items, false);
+		selectionSlot.GetComponent<Transform>().localScale = new Vector3(slotSize, slotSize, 0);
+		selectionSlot.GetComponent<SpriteRenderer>().color = selectionSlotColor;
+		selectionSlot.SetActive(false);
+	}
+}
